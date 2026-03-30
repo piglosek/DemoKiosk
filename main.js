ff5438d5952303ef91c890ab3b790843da0c0f82
piglosek <piglosek@cisco.com>
2026-03-30 17:52:09 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ function setup() {
   Alpine.store('model', {
     currentPage: 'home', // 'home', 'service'
     currentLanguage: 'polish',
-    dialNumber1: 'sip:rtbc@davijennbootcampint-1xbd.rooms.webex.com',
-    dialNumber2: 'sip:piglosek.cisco@webex.com',
-    dialNumber3: 'tel:1112',
+    dialNumber1: 'rtbc@davijennbootcampint-1xbd.rooms.webex.com',
+    dialNumber2: 'piglosek.cisco@webex.com',
+    dialNumber3: '1112',
     services: [],
 
     init() {
