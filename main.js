77f4f3249327a869aa8e864e18f7d88f1f1133eb
piglosek <piglosek@cisco.com>
2026-04-02 16:33:12 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ function setup() {
   Alpine.store('model', {
     currentPage: 'home', // 'home', 'service'
     currentLanguage: 'polish',
-    dialNumber1: 'rtbc@davijennbootcampint-1xbd.rooms.webex.com',
-    dialNumber2: 'piglosek.cisco@webex.com',
+    dialNumber1: 'piglosek.cisco@webex.com',
+    dialNumber2: 'rtbc@davijennbootcampint-1xbd.rooms.webex.com',
     dialNumber3: '1112',
     services: [],
 
@@ -15,9 +15,9 @@ function setup() {
         this.dialNumber = params.get('number');
       }
       this.services = [
-        { url: this.dialNumber1, name: 'Obsługa Kredytów' },
+        { url: this.dialNumber1, name: 'Zespół Doradczy' },
         { url: this.dialNumber2, name: 'Private Banking' },
-        { url: this.dialNumber3, name: 'Konsultacja z Agentem' },
+        { url: this.dialNumber3, name: 'Konsultacja Kredytowa' },
       ];
     },
     get page() {
